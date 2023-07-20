30e4b500998984b58c3eaf503fa95913b13f9648
Brian Alejandro Ochoa Duran <brian16ochoa@gmail.com>
2023-07-20 12:23:42 -0600

    feat(): adding entities

---
+++ src/main/java/com/platzi/market/persistence/entity/Casting.java
@@ -0,0 +1,15 @@
+package com.platzi.market.persistence.entity;
+
+public class Casting {
+    public static void main(String[] args) {
+        int numeroEntero = 12;
+        double numeroDouble = numeroEntero;
+        int numeroEnteroDouble = (int) numeroDouble;
+
+        System.out.println("numeroEntero, "+numeroEntero);
+        System.out.println("numeroDouble, "+numeroDouble);
+        System.out.println("numeroEnteroDouble, "+numeroEnteroDouble);
+
+
+    }
+}
